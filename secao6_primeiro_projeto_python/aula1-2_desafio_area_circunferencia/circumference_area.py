9c81985e7f325cd563ec16d06f5cdd9d240d0db5
luizfernando <luiz-fsilva37@hotmail.com>
2020-05-17 18:28:59 -0300

    Adição seção 6 aulas 1-3

---
+++ secao6_primeiro_projeto_python/aula1-2_desafio_area_circunferencia/circumference_area.py
@@ -0,0 +1,5 @@
+PI = 3.14159
+radius = 15.3
+circumference_area = PI * (radius ** 2)
+
+print(f'Circumference area is: %.2f' % (circumference_area))
---
+++ secao6_primeiro_projeto_python/aula3_encoding/encoding.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+PI = 3.14159
+radius = 15.3
+circumference_area = PI * (radius ** 2)
+
+print(f'A área da circunferência é: %.2f' % (circumference_area))
