276efee26542281e6520168d41e6dfa1dc4f9150
luizfernando <luiz-fsilva37@hotmail.com>
2020-05-23 17:37:08 -0300

    Atualização seção 9 aula 5

--- secao9_manipulacao_arquivos/aula5_usando_try_finally/using_try_finally.py
+++ secao9_manipulacao_arquivos/aula5_usando_try_finally/using_try_finally.py
@@ -4,7 +4,7 @@ try:
     my_file = open('..\\aula1_criando_arquivo_CSV\\people.csv')
 
     for register in my_file:
-        print('Name: {}, age: {}'.format(register.strip().split(',')))
+        print('Name: {}, age: {}'.format(*register.strip().split(',')))
 
 finally:
     my_file.close()
