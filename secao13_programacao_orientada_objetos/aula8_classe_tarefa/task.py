6d3d6860d82c188474057309a2821ebfe4db9a65
luizfernando <luiz-fsilva37@hotmail.com>
2020-06-14 17:13:40 -0300

    Adição seção 13 aula 8

---
+++ secao13_programacao_orientada_objetos/aula8_classe_tarefa/task.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+class Task:
+    def __init__(self, description):
+        self.description = description
+        self.done = False
+        self.date = datetime.now()
+
+    def completed(self):
+        self.done = True
+
+    def __str__(self):
+        return self.description + ' ' + ('(Done)' if self.done else '')
+
+
+def main():
+    house = []
+    house.append(Task('To watch anime'))
+    house.append(Task('To study programming'))
+    house.append(Task('To play game'))
+    house.append(Task('To read book'))
+
+    [task.completed() for task in house if task.description == 'To watch anime']
+    for task in house:
+        print(f'- {task}')
+
+
+if __name__ == "__main__":
+    main()
