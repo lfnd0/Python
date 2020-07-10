13fca040ad7ed901489b82eee0b4d360f4c1da02
luizfernando <luiz-fsilva37@hotmail.com>
2020-07-10 14:03:37 -0300

    Atualização seção 18 aula 10

--- secao18_banco_dados/aula10_criar_tabela_estado/create_state_table.sql
+++ secao18_banco_dados/aula10_criar_tabela_estado/create_state_table.sql
@@ -5,8 +5,8 @@ CREATE TABLE estado (
     nome VARCHAR(45) NOT NULL,
     sigla VARCHAR(2) NOT NULL,
     regiao ENUM('Norte', 'Nordeste', 'Centro-oeste', 'Sudeste', 'Sul') NOT NULL,
-    populacao DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (id),
+    populacao DECIMAL(5, 2) NOT NULL,
+    PRIMARY KEY(id),
     UNIQUE KEY(nome),
     UNIQUE KEY(sigla)
 );
